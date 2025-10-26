c0c121e3ff7b56dd2371cdc7be15b389eb32378c
Alireza <golshandeveloper@gmail.com>
2025-10-26 16:25:24 +0330

    changed the database to mysql

--- dijkstra/settings.py
+++ dijkstra/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = "dijkstra.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE' : 'django.db.backends.mysql' ,
+        'NAME' : 'shortestpathfinder' ,
+        'USER' : 'root' ,
+        'PASSWORD' : 'password' ,
+        'HOST' : "127.0.0.1" ,
+        'PORT' : '3306'
     }
 }
 
