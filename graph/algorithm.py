971b0029cfa8d386fa5d932147839919ea60e423
Alireza <golshandeveloper@gmail.com>
2025-10-30 18:41:42 +0330

    modified the algorithm to recontruct the path after finding the shortest distance

--- graph/algorithm.py
+++ graph/algorithm.py
@@ -1,39 +1,3 @@
-# from .priorityqueue import PriorityQueue
-# from .models import Node, Edge
-#
-#
-# def dijkstrasalgorithm(graph_obj, start_node_id):
-#     adjacency = {node.id: [] for node in graph_obj.nodes.all()}
-#     for edge in graph_obj.edges.all():
-#         adjacency[edge.from_node.id].append((edge.to_node.id, edge.weight))
-#         adjacency[edge.to_node.id].append((edge.from_node.id, edge.weight))
-#
-#     distances = {node_id: float('inf') for node_id in adjacency}
-#     distances[start_node_id] = 0
-#     visited = set()
-#
-#     pq = PriorityQueue()
-#     pq.push((0, start_node_id))
-#
-#     while len(pq) > 0:
-#         current_dist, current_node = pq.pop()
-#
-#         if current_node in visited:
-#             continue
-#
-#         visited.add(current_node)
-#
-#         for neighbor, weight in adjacency[current_node]:
-#             distance = current_dist + weight
-#
-#             if distance < distances[neighbor]:
-#                 distances[neighbor] = distance
-#                 pq.push((distance, neighbor))
-#
-#     return distances
-
-
-
 
 from .priorityqueue import PriorityQueue
 from .models import Node, Edge
@@ -46,7 +10,7 @@ def dijkstrasalgorithm(graph_obj, start_node_name, end_node_name=None):
         adjacency[edge.from_node.name].append((edge.to_node.name, edge.weight))
         adjacency[edge.to_node.name].append((edge.from_node.name, edge.weight))
 
-    # CHANGED: use node names for distances and previous_nodes
+
     distances = {name: float('inf') for name in adjacency}
     previous_nodes = {name: None for name in adjacency}
     distances[start_node_name] = 0
@@ -62,9 +26,9 @@ def dijkstrasalgorithm(graph_obj, start_node_name, end_node_name=None):
             continue
         visited.add(current_node)
 
-        # CHANGED: early stop if we reached destination
-        if end_node_name is not None and current_node == end_node_name:
-            break
+
+        # if end_node_name is not None and current_node == end_node_name:
+        #     break
 
         for neighbor, weight in adjacency[current_node]:
             distance = current_dist + weight
@@ -73,16 +37,18 @@ def dijkstrasalgorithm(graph_obj, start_node_name, end_node_name=None):
                 previous_nodes[neighbor] = current_node
                 pq.push((distance, neighbor))
 
-    # CHANGED: reconstruct path using node names
-    path = []
-    if end_node_name is not None:
-        current = end_node_name
-        while current is not None:
-            path.insert(0, current)
-            current = previous_nodes[current]
 
-    # CHANGED: return path and distances using node names
+    all_paths= {}
+    for node_name in adjacency:
+        path = []
+        current = node_name
+        while current is not None:
+            path.insert(0 , current)
+            current = previous_nodes[current]
+        all_paths[node_name] = path
+
+
     return {
         "distances": distances,
-        "path": path if end_node_name else None
+        "paths": all_paths
     }
--- graph/views.py
+++ graph/views.py
@@ -13,19 +13,7 @@ class GraphViewSet(viewsets.ModelViewSet):
     queryset = Graph.objects.all()
     serializer_class = GraphSerializer
 
-    # @action(detail=True , methods=['post'])
-    # def shortest_path(self, request , pk=None):
-    #     graph = self.get_object()
-    #     start_node_id = request.data.get('start_node_id')
-    #
-    #     if not start_node_id:
-    #         return Response({"error" : "start_node_id is required."} , status = status.HTTP_400_BAD_REQUEST)
-    #
-    #     try :
-    #         result = dijkstrasalgorithm(graph , int(start_node_id))
-    #     except Exception as e :
-    #         return Response({'error' : str(e)} , status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    #     return Response({"Distances":result})
+
     @action(detail=True, methods=['post'])
     def shortest_path(self, request, pk=None):
         graph = self.get_object()
